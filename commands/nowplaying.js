d6dd0d5e22d6c9c9d73f70a29dc046555f961f8a
Robert Macaibay <rtmacaibay@usfca.edu>
2020-04-20 11:32:47 -0700

    Added nowplaying function

---
+++ commands/nowplaying.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: 'nowplaying',
+    description: 'Get the song that is playing',
+    alias: ['np'],
+    args: false,
+	execute(message, args) {
+		const serverQueue = message.client.queue.get(message.guild.id);
+        if (!serverQueue) 
+            return message.channel.send('There is nothing playing.');
+		return message.channel.send(`Now playing: ${serverQueue.songs[0].title}`);
+	}
+};
