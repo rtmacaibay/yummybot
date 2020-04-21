6462a6f1b71c3ec8eb8c67c23bbaf529bc66abd6
Robert Macaibay <rtmacaibay@usfca.edu>
2020-04-20 18:57:28 -0700

    Fixes to Queue and Remove

--- commands/queue.js
+++ commands/queue.js
@@ -6,7 +6,8 @@ module.exports = {
         const serverQueue = message.client.queue.get(message.guild.id);
         let output = 'Now Playing: ';
         for (var i in serverQueue.songs) {
-            output += `**${i+1}: ${serverQueue.songs[i].title}**\n`;
+            var pos = i+1;
+            output += `**${pos}: ${serverQueue.songs[i].title}**\n`;
         }
 
         return message.channel.send(output);
--- commands/remove.js
+++ commands/remove.js
@@ -12,13 +12,17 @@ module.exports = {
             return message.channel.send('There is no song that I could skip!');
         if (args[0] < 2 || args[0] > serverQueue.songs.length)
             return message.channel.send(`I can't remove a song at position ${args[0]}!`);
-
+        
         var songs = [];
         var song_index = 0;
         var song_removed;
         for (var i in serverQueue.songs)
+            const new_song = {
+                title: serverQueue.songs[i].title,
+                url: serverQueue.songs[i].url
+            };
             if (i != args[0]-1)
-                songs[song_index++] += serverQueue.songs[i];
+                songs[song_index++] += new_song;
             else 
                 song_removed = serverQueue.songs[i];
 
