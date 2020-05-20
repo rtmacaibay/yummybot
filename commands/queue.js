8a760cf9f2f14fafbaba46c2f97f84141f280986
Robert Macaibay <rtmacaibay@usfca.edu>
2020-05-19 19:16:53 -0700

    More Queue Testing

--- commands/queue.js
+++ commands/queue.js
@@ -21,27 +21,30 @@ module.exports = {
             
             message.channel.send(this.createQueueEmbed(serverQueue, index))
             .then( (newMessage) => {
-                newMessage.react('⬅️').then(newMessage.react('➡️'));
-                let forward = newMessage.createReactionCollector( (reaction, user) => 
-                    reaction.emoji.name === '➡️' && !user.bot, { time: 120000 });
-                let back = newMessage.createReactionCollector( (reaction, user) => 
-                    reaction.emoji.name === '⬅️' && !user.bot, { time: 120000 });
+                newMessage.react('⬅️')
+                .then(newMessage.react('➡️')
+                .then( () => {
+                    let forward = newMessage.createReactionCollector( (reaction, user) => 
+                        reaction.emoji.name === '➡️' && !user.bot, { time: 120000 });
+                    let back = newMessage.createReactionCollector( (reaction, user) => 
+                        reaction.emoji.name === '⬅️' && !user.bot, { time: 120000 });
 
-                forward.on('collect', () => {
-                    index += 10;
-                    
-                    if (index > serverQueue.songs.length - 1) index = 0;
+                    forward.on('collect', () => {
+                        index += 10;
+                        
+                        if (index > serverQueue.songs.length - 1) index = 0;
 
-                    newMessage.edit(this.createQueueEmbed(serverQueue, index));
-                });
+                        newMessage.edit(this.createQueueEmbed(serverQueue, index));
+                    });
 
-                back.on('collect', () => {
-                    index -= 10;
+                    back.on('collect', () => {
+                        index -= 10;
 
-                    if (index < 0) index = (serverQueue.songs.length > 9 ? serverQueue.songs.length - 10 : 0);
+                        if (index < 0) index = (serverQueue.songs.length > 9 ? serverQueue.songs.length - 10 : 0);
 
-                    newMessage.edit(this.createQueueEmbed(serverQueue, index));
-                });
+                        newMessage.edit(this.createQueueEmbed(serverQueue, index));
+                    });
+                }));
             });
         }
     },
