ba14cc13865f1d612b5ad819bf6ae80687b5c27c
Robert Macaibay <rtmacaibay@usfca.edu>
2020-05-21 02:22:26 -0700

    async fix

--- acnh/pull_acnh.js
+++ acnh/pull_acnh.js
@@ -1,7 +1,7 @@
 const fetch = require('fetch');
 
 module.exports = {
-    pull_bugs() {
+    async pull_bugs() {
         await fetch('http://acnhapi.com:1304/v1/bugs')
         .then(response => response.json())
         .then(data => console.log(data));
