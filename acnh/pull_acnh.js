c3b3a674c641a9eea3cf7251fd4824c33ff2b285
Robert Macaibay <rtmacaibay@usfca.edu>
2020-05-21 02:34:21 -0700

    Fix to pull_bugs

--- acnh/pull_acnh.js
+++ acnh/pull_acnh.js
@@ -4,6 +4,6 @@ module.exports = {
     async pull_bugs() {
         await fetch(`http://acnhapi.com:1304/v1/bugs`)
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(json => console.log(json));
     }
 };
