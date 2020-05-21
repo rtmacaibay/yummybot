dfce37c4f44bd221a9da79a9d49cdad3ea373e8e
Robert Macaibay <rtmacaibay@usfca.edu>
2020-05-20 19:26:06 -0700

    Intents fix

--- client/client.js
+++ client/client.js
@@ -2,7 +2,7 @@ const { Client, Collection, Intents } = require('discord.js');
 
 module.exports = class extends Client {
 	constructor(config) {
-		super({ws: { intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_VOICE_STATES'] }});
+		super({ws: { intents: Intents.NON_PRIVILEGED }});
 
 		this.commands = new Collection();
 
