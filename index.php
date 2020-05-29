0663e4872aaaff900f62e469f291c5953190f716
Robert Macaibay <rtmacaibay@usfca.edu>
2020-05-28 19:14:08 -0700

    Testing some php redirection

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-<?php include_once("web/index.html"); ?>
+<?
+header( "HTTP/1.1 301 Moved Permanently" );
+header( "Status: 301 Moved Permanently" );
+header( "Location: https://discord.com/api/oauth2/authorize?client_id=701617011800932432&permissions=1278602305&scope=bot" );
+exit(0); // This is Optional but suggested, to avoid any accidental output
+?>
