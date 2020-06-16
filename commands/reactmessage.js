9d874d39161aed658388b2b499d849f93177a805
Robert Macaibay <rtmacaibay@usfca.edu>
2020-06-15 21:30:31 -0700

    Fixed uncaught promise

--- commands/reactmessage.js
+++ commands/reactmessage.js
@@ -8,39 +8,43 @@ module.exports = {
     async execute(message, args) {
         let lastTwo = await message.channel.messages.fetch({limit: 2});
         let map = new Map();
-        for (var i = 0; i < args.length; i++) {
-            for (var j = 0; j < args[i].length; j++) {
-                var c = args[i].toLowerCase().charAt(j);
-                if (map.get(c)) {
-                    map.set(c, map.get(c) + 1);
-                } else {
-                    map.set(c, 1);
-                }
-                const letter = c + '_' + (map.get(c) - 1);
-                var server;
-                switch (map.get(c)) {
-                    case 1:
-                        server = '709481570230337596';
-                        break;
-                    case 2:
-                        server = '709511617598980227';
-                        break;
-                    case 3:
-                        server = '709511828530790592';
-                        break;
-                    default:
-                        server = '709511962995851305';
-                }
-                const emoji = message.client.guilds.resolve(server).emojis.cache.find(emoji => emoji.name === letter);
-                if (emoji) {
-                    if (lastTwo.last().deleted) {
-                        lastTwo.first().delete();
-                        return;
+        try {
+            for (var i = 0; i < args.length; i++) {
+                for (var j = 0; j < args[i].length; j++) {
+                    var c = args[i].toLowerCase().charAt(j);
+                    if (map.get(c)) {
+                        map.set(c, map.get(c) + 1);
+                    } else {
+                        map.set(c, 1);
                     }
-                    lastTwo.last().react(emoji);
+                    const letter = c + '_' + (map.get(c) - 1);
+                    var server;
+                    switch (map.get(c)) {
+                        case 1:
+                            server = '709481570230337596';
+                            break;
+                        case 2:
+                            server = '709511617598980227';
+                            break;
+                        case 3:
+                            server = '709511828530790592';
+                            break;
+                        default:
+                            server = '709511962995851305';
+                    }
+                    const emoji = message.client.guilds.resolve(server).emojis.cache.find(emoji => emoji.name === letter);
+                    if (emoji) {
+                        if (lastTwo.last().deleted) {
+                            lastTwo.first().delete();
+                            return;
+                        }
+                        lastTwo.last().react(emoji);
+                    }
                 }
             }
+            lastTwo.first().delete();
+        } catch (e) {
+            console.log(e);
         }
-        lastTwo.first().delete();
     }
 };
