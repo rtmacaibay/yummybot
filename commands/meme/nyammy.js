77e3b93ab67639022e6bf76fe6dbfc9e43366c7e
Robert Macaibay <rtmacaibay@usfca.edu>
2020-11-02 23:46:41 -0800

    Some Adjustments

--- commands/meme/nyammy.js
+++ commands/meme/nyammy.js
@@ -6,13 +6,13 @@ module.exports = {
         var output = "";
 
         if (!args.length) {
-            output = output.concat('ⓝⓨⓐ ⓑⓐbⓔ!');
+            output = output.concat('ⓝⓨⓐ~~ babe!');
         } else if (args[0] > 165) {
             message.channel.send(`${message.author}, you can only nyammy up to 165 times.`);
             return;
         } else {
             for (var i = 0; i < args[0]; i++) {
-                output = output.concat ('ⓝⓨⓐ ⓑⓐbⓔ! ');
+                output = output.concat ('ⓝⓨⓐ~~ babe!');
             }
         }
         
